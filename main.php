d0654bcd2da3baa97aa4e6299a1a7d05ea1b38c2
William D <wdonayredroid@gmail.com>
2024-01-08 16:08:02 +0800

    - update 2

---
+++ main.php
@@ -0,0 +1,30 @@
+<?php 
+
+  /*
+    Plugin Name: Wordpress OpenStreetMap
+    Description: Dilate OpenStreetMap
+    Author: William Donayre Jr - Dilate Digital LLC. <william@dilate.com.au>
+    Text Domain: wp-openstreetmap
+    Version: 0.0.1
+  */
+
+    require 'wp-openstreetmap.php';
+    use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+    $myUpdateChecker = PucFactory::buildUpdateChecker(
+        'https://github.com/wdonayre/wp-openstreetmap/',
+        __FILE__,
+        'wp-openstreetmap'
+    );
+
+    //Set the branch that contains the stable release.
+    $myUpdateChecker->setBranch('master');
+
+    //Optional: If you're using a private repository, specify the access token like this:
+    // $myUpdateChecker->setAuthentication('your-token-here');
+
+
+  // If this file is called directly, abort.
+  if (!defined('WPINC')) {
+    die;
+  }
--- wp-openstreetmap.php
+++ wp-openstreetmap.php
@@ -11,17 +11,3 @@ require dirname(__FILE__) . '/load-v5p3.php';
 
 
 
-require 'plugin-update-checker/plugin-update-checker.php';
-use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
-
-$myUpdateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/wdonayre/wp-openstreetmap/',
-	__FILE__,
-	'wp-openstreetmap'
-);
-
-//Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('master');
-
-//Optional: If you're using a private repository, specify the access token like this:
-// $myUpdateChecker->setAuthentication('your-token-here');
