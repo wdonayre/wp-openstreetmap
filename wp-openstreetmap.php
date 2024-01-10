50c1ad5afd85a28d03fdb60f6cac6aedcc2368f8
William D <wdonayredroid@gmail.com>
2024-01-11 01:00:59 +0800

    - update

--- wp-openstreetmap.php
+++ wp-openstreetmap.php
@@ -1,13 +1,32 @@
-<?php
-/**
- * Plugin Update Checker Library 5.3
- * http://w-shadow.com/
- *
- * Copyright 2022 Janis Elsts
- * Released under the MIT license. See license.txt for details.
- */
+<?php 
 
-require dirname(__FILE__) . '/load-v5p3.php';
+  /*
+    Plugin Name: Wordpress OpenStreetMap
+    Description: Dilate OpenStreetMap
+    Author: William Donayre Jr - Dilate Digital LLC. <william@dilate.com.au>
+    Text Domain: wp-openstreetmap
+    Version: 0.0.0
+  */
 
+    // If this file is called directly, abort.
+    if (!defined('WPINC')) {
+        die;
+    }
+        
+
+    require 'puc/plugin-update-checker.php';
+    use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+    $myUpdateChecker = PucFactory::buildUpdateChecker(
+        'https://github.com/wdonayre/wp-openstreetmap/',
+        __FILE__,
+        'wp-openstreetmap'
+    );
+
+    //Set the branch that contains the stable release.
+    $myUpdateChecker->setBranch('main');
+
+    //Optional: If you're using a private repository, specify the access token like this:
+    // $myUpdateChecker->setAuthentication('your-token-here');
 
 
